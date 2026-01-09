f9ee2cec984590f73073416b7a83f03a316fb465
Ryan Beatty <73397851+rdgbeatty@users.noreply.github.com>
2026-01-09 17:03:20 -0500

    I'm dumb

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,5 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command }) => ({
   plugins: [react(), tsconfigPaths()],
-  base: command === "serve" ? "/" : "/YOUR_REPO_NAME/",
+  base: command === "serve" ? "/" : "/react-parse-goonmetrics/",
 }));
